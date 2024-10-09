12be008c87946eedc6015e87867575634aa2dec2
Adetola Adewodu <adetola.adewodu@gmail.com>
2024-10-09 18:06:54 -0400

    Adding url to get countries

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+export default async function Page() {
+    let data = await fetch('http://localhost:8080/countries')
+    let countries = await data.json()
+    return (
+        <ul>
+        {
+            <li key={countries.id}>{countries.name}</li>
+        }
+        </ul>
+    )
   }
